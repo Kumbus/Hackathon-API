7fe3a4eadc7771ee84efcff36362417860fbd4d3
WikarNotAvailable <wikar8998@gmail.com>
2022-12-03 00:19:51 +0100

    token

--- TaskControllerAPI/Application/Interfaces/ITokenService.cs
+++ TaskControllerAPI/Application/Interfaces/ITokenService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.Dtos.UserDtos;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,8 @@ using System.Threading.Tasks;
 
 namespace Application.Interfaces
 {
-    internal interface ITokenService
+    public interface ITokenService
     {
+        public string GenerateJWT(LoginResponseDto user, IConfiguration config);
     }
 }
--- TaskControllerAPI/Application/Interfaces/IUsersService.cs
+++ TaskControllerAPI/Application/Interfaces/IUsersService.cs
@@ -1,4 +1,4 @@
-﻿using Application.Dtos;
+﻿using Application.Dtos.UserDtos;
 using Domain.Entities;
 using Microsoft.AspNetCore.Identity;
 using System;
