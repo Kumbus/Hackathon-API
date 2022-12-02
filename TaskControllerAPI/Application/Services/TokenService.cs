4d5731fc15a2b6919d3b0ce43a4ee21866900ce9
WikarNotAvailable <wikar8998@gmail.com>
2022-12-03 00:41:50 +0100

    finished jwt

--- TaskControllerAPI/Application/Services/TokenService.cs
+++ TaskControllerAPI/Application/Services/TokenService.cs
@@ -1,12 +1,38 @@
-﻿using System;
+﻿using Application.Dtos.UserDtos;
+using Application.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Services
 {
-    internal class TokenService
+    internal class TokenService : ITokenService
     {
+        public string GenerateJWT(LoginResponseDto user, IConfiguration config)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[] {
+                new Claim(ClaimTypes.NameIdentifier, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim("Date", DateTime.Now.ToString())
+            };
+
+            var token = new JwtSecurityToken(config["Jwt:Issuer"],
+              config["Jwt:Issuer"],
+              claims,
+              expires: DateTime.Now.AddMinutes(120),
+              signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
     }
 }
