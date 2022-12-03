39fe54af72f20562f310c1a414a7539905ae73db
WikarNotAvailable <wikar8998@gmail.com>
2022-12-03 02:18:24 +0100

    added mailing

---
+++ TaskControllerAPI/WebApi/Installers/MailInstaller.cs
@@ -0,0 +1,14 @@
+﻿using EmailService;
+
+namespace WebApi.Installers
+{
+    public class MailInstaller : IInstaller
+    {
+        public void InstallServices(WebApplicationBuilder builder)
+        {
+            var emailConfig = builder.Configuration.GetSection("EmailConfiguration").Get<EmailConfiguration>();
+            builder.Services.AddSingleton(emailConfig);
+            builder.Services.AddScoped<IEmailSender, EmailSender>();
+        }
+    }
+}
