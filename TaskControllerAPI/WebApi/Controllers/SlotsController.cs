6bf4548a537ada818278f3608bf029f73b4f9e56
WikarNotAvailable <wikar8998@gmail.com>
2022-12-03 02:08:02 +0100

    finished slots controller

--- TaskControllerAPI/WebApi/Controllers/SlotsController.cs
+++ TaskControllerAPI/WebApi/Controllers/SlotsController.cs
@@ -5,23 +5,51 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApi.Controllers
 {
-    
+
     [Route("slots")]
     [ApiController]
     public class SlotsController : ControllerBase
     {
         private readonly ISlotsService _slotsService;
 
-        public SlotsController (ISlotsService slotsService)
+        public SlotsController(ISlotsService slotsService)
         {
             _slotsService = slotsService;
         }
 
         [HttpPost]
-        public async Task <IActionResult> AddSlot(PostSlotDto newSlot)
+        public async Task<IActionResult> AddSlot(PostSlotDto newSlot)
         {
-            await _slotsService.AddSlotAsync(newSlot);
-            return Ok(newSlot);
+            var slot = await _slotsService.AddSlotAsync(newSlot);
+            return Ok(slot);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSlot(Guid id)
+        {
+           var slot = await _slotsService.GetSlotByIdAsync(id);
+            return Ok(slot);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetSlots(string userId)
+        {
+            var slots = await _slotsService.GetSlotsAsync(userId);
+            return Ok(slots);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateSlot(UpdateSlotDto updatedSlot, Guid id)
+        {
+            var slot = await _slotsService.UpdateSlotAsync(updatedSlot, id);
+            return Ok(slot);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSlot(Guid id)
+        {
+            await _slotsService.DeleteSlotAsync(id);
+            return Ok();
         }
     }
     
